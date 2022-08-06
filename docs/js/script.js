e554123eef7fd488aa3c0eaef3ae1ff5c3a070e4
Volodymyr Shulgach <shulgach2013@gmail.com>
2022-08-06 15:09:57 +0300

    full page finished, going to adaptive

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,1 @@
-$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0})}));
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,autoplay:!0,autoplaySpeed:1e4,fade:!0,speed:1e3,infinite:!0,cssEase:"ease-in-out",touchThreshold:300})}));
--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
     $('.slider').slick({
         arrows: false,
         dots: true,
+        autoplay:true,
+        autoplaySpeed: 10000,
+        fade: true,
+        speed: 1000,
+        infinite: true,
+        cssEase: 'ease-in-out',
+        touchThreshold: 300
     });
-
 });
